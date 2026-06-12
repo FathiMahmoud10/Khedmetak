4a25114339b434b9366f50a4623ad5bc6164107f
Aya Ahmed <ayabelal906@gmail.com>
2026-06-12 07:17:11 +0300

    Modify ChatSession Entity to be created without user login

--- Khedmetak.DAL/Entities/ChatSession .cs
+++ Khedmetak.DAL/Entities/ChatSession .cs
@@ -9,16 +9,16 @@ namespace Khedmetak.DAL.Entities
 {
     public class ChatSession : BaseEntity
     {
-        public DateTime StartedAt { get; set; }
+        public DateTime StartedAt { get; set; } = DateTime.Now; // add initial date to it
         public DateTime? EndedAt { get; set; }
 
         #region Foreign Keys
-        public int UserId { get; set; }
+        public int? UserId { get; set; } // make it nullable
         public int? CategoryId { get; set; }
         #endregion
 
         #region Relations
-        public User User { get; set; } = null!;
+        public User? User { get; set; } = null!; // make it nullable
         public Category? Category { get; set; }
         public ICollection<ChatMessage> ChatMessages { get; set; } = new List<ChatMessage>();
         public ICollection<UserDocument> UserDocuments { get; set; } = new List<UserDocument>();
--- Khedmetak.DAL/Migrations/20260612041603_ModifyChatSession.Designer.cs
+++ Khedmetak.DAL/Migrations/20260612041603_ModifyChatSession.Designer.cs
@@ -4,6 +4,7 @@ using Khedmetak.Core.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Khedmetak.DAL.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260612041603_ModifyChatSession")]
+    partial class ModifyChatSession
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
