0907f9e959abbcbe29aa466f5b2e85c19adec883
Aya Ahmed <ayabelal906@gmail.com>
2026-06-20 21:29:04 +0300

    Add Embedding service

---
+++ Khedmetak.AI/Services/Abstraction/IEmbeddingService.cs
@@ -0,0 +1,19 @@
+﻿using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using Khedmetak.AI.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Khedmetak.AI.Services.Abstraction
+{
+    public interface IEmbeddingService
+    {
+       
+
+        public  Task<float[]> GenerateEmbeddingAsync(string text);
+
+
+    }
+}
---
+++ Khedmetak.AI/Services/Implementation/EmbeddingService.cs
@@ -0,0 +1,40 @@
+﻿using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using Khedmetak.AI.Configuration;
+using Khedmetak.AI.DTOs;
+using Khedmetak.AI.Services.Abstraction;
+using Khedmetak.DAL.Entities;
+using Microsoft.Extensions.Options;
+using OpenAI;
+using OpenAI.Embeddings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Khedmetak.AI.Services.Implementation
+{
+    public class EmbeddingService :IEmbeddingService
+    {
+        private readonly EmbeddingClient _embeddingClient;
+
+        public EmbeddingService(OpenAIClient openAIClient, IOptions<AISettings> settings)
+        {
+            _embeddingClient = openAIClient.GetEmbeddingClient(settings.Value.EmbeddingModel);
+
+        }
+
+        public async Task<float[]> GenerateEmbeddingAsync(string text)
+        {
+            EmbeddingGenerationOptions options = new()
+            {
+                Dimensions = 768 // 1536 full size
+            };
+            OpenAIEmbedding embedding = await _embeddingClient.GenerateEmbeddingAsync(text,options);
+
+            return embedding.ToFloats().ToArray();
+        }
+
+    
+    }
+}
